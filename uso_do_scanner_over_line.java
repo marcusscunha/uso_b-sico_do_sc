903d68dc1684db36fa3bba333de6a497864e60b7
Marcus Vitor da Cunha Nascimento <106033361+marcusscunha@users.noreply.github.com>
2022-06-17 14:55:16 -0300

    Add files via upload

---
+++ uso_do_scanner_over_line.java
@@ -0,0 +1,31 @@
+package aplicacao;
+
+
+
+
+import java.util.Scanner;
+
+	public class uso_do_scanner_over_line{
+
+
+		public static void main(String[] args) {
+			
+			Scanner sc= new Scanner (System.in);
+			int x;
+			String s1,s2,s3;
+			
+			x=sc.nextInt();
+			sc.nextLine();
+			s1=sc.nextLine();
+			s2=sc.nextLine();
+			s3=sc.nextLine();
+			System.out.println("voce digitou:");
+			System.out.println(s1);
+			System.out.println(s2);
+			System.out.println(s3);
+			sc.close();
+		}
+
+	}
+
+
