6dc1e89b5eaf351a1c88ade56f901a060a4a53fd
Marcus Vitor da Cunha Nascimento <106033361+marcusscunha@users.noreply.github.com>
2022-06-17 14:24:05 -0300

    Add files via upload

---
+++ uso_do_scanner_flut_ponto.java
@@ -0,0 +1,25 @@
+package aplicacao;
+
+
+	import java.util.Locale;
+import java.util.Locale.Category;
+import java.util.Scanner;
+
+	public class uso_do_scanner_flut_ponto {
+
+		
+
+		public static void main(String[] args) {
+			Locale.setDefault(Locale.US);
+			Scanner sc= new Scanner (System.in);
+
+			double x;
+			x=sc.nextDouble();
+			System.out.printf("voce digitou: %.9f%n",+x);
+
+			sc.close();
+		}
+
+	}
+
+
